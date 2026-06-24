886c262c376bbd45d03ed6f6682d3c6b86a82005
Mike Tsakiroglou <32255755+MikeTsak@users.noreply.github.com>
2026-06-24 16:52:23 +0300

    feat: initialize axios instance with token interceptor in constants/api.js

--- constants/api.js
+++ constants/api.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // ⚠️ REPLACE THIS WITH YOUR COMPUTER'S LOCAL WI-FI IP ADDRESS!
 // Example: 'http://192.168.1.15:3001/api'
-const BASE_URL = 'https://vtm.back.miketsak.gr/api'; 
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL; 
 
 const api = axios.create({
   baseURL: BASE_URL,
